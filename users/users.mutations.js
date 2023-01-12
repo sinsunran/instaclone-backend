6904822c90547ef7a66d08fedc8ef6b93344a315
sinsunran <115200753+sinsunran@users.noreply.github.com>
2023-01-12 11:05:33 +0900

    apply try and catch

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,24 +7,35 @@ export default {
       _,
       { username, email, name, location, avatarURL, gihubUsername, password }
     ) => {
-      const existUser = await client.user.findFirst({
-        where: { OR: [{ username }, { email }, { gihubUsername }] },
-      });
-      if (existUser) {
-        throw new Error("$error");
+      try {
+        const existUser = await client.user.findFirst({
+          where: { OR: [{ username }, { email }, { gihubUsername }] },
+        });
+        if (existUser) {
+          throw new Error("$error");
+        }
+        const hashedPassword = await bcrypt(password, 5);
+        const user = await client.user.create({
+          data: {
+            username,
+            email,
+            name,
+            location,
+            avatarURL,
+            githubUsername,
+            password: hashedPassword,
+          },
+        });
+        return {
+          ok: true,
+          id: user.id,
+        };
+      } catch (e) {
+        return {
+          ok: false,
+          error: e.message,
+        };
       }
-      const hashedPassword = await bcrypt(password, 5);
-      return client.user.create({
-        data: {
-          username,
-          email,
-          name,
-          location,
-          avatarURL,
-          githubUsername,
-          password: hashedPassword,
-        },
-      });
     },
   },
 };
